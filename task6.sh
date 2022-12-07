ba6c705faa4419d42e47405836f507cc1f454745
ErshovDim <89608262+ErshovDim@users.noreply.github.com>
2022-12-07 15:30:22 +0100

    Create task6.sh

---
+++ task6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+xdg-user-dirs-update
+groupadd sudoers
+for i in {1..5}
+do
+  mkdir "folder$i"
+done
+cat users.txt | for i in {1..5} 
+do
+  read y
+  useradd -m  $y
+  usermod -aG sudo $y
+done
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install ufw -y
+sudo apt install nginx -y
+sudo apt-get install git-all -y
+sudo apt-get install -y net-tools
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo docker run hello-world
