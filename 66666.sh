160ed6b0dcc47ee274cd72578d5c570c96b07a30
ErshovDim <89608262+ErshovDim@users.noreply.github.com>
2022-12-07 14:58:58 +0100

    Create 66666.sh

---
+++ 66666.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+xdg-user-dirs-update
+groupadd sudoers
+for i in {1..5}
+do
+  mkdir "folder$i"
+done
+cat users.txt | for i in {1..5} 
+do
+  read y
+  useradd -m -G sudoers $y
+  usermod -aG sudo $y
+done
